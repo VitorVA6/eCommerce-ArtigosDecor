98e129023f26834e1f5131eaebd39d6eb9c8c60b
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-07-24 22:23:34 -0300

    refactor: remaking nav lateral in client, adding animation, changing styles and fixing responsiveness

---
+++ eCommerce/src/components/CategoryItem.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
+import {BsArrowRight} from 'react-icons/bs'
+
+export default function CategoryItem({categoria}) {
+
+    const [show, setSHow] = useState(false)
+    const [translate, setTranslate] = useState('')
+    const [opacity, setOpacity] = useState(0)
+
+    useEffect( () =>{
+        setTranslate(show === true && '-translate-x-2')
+    }, [show] )
+
+    return (
+        <Link  
+            to={`/category/${categoria._id}`} 
+            className='flex flex-col items-center gap-y-3'
+            onMouseOver={() => {
+                setSHow(true)
+                setOpacity(100)
+            }}
+            onMouseLeave={() => {
+                setSHow(false)
+                setOpacity(0)
+            }}
+            >
+            <div className='p-2 md:p-[11px] lg:p-2 bg-white rounded-[70px]'>
+                <div 
+                    className={`w-[120px] h-[120px] md:w-40 md:h-40 lg:h-36 lg:w-36 xl:w-32 xl:h-32 bg-cover bg-center rounded-full transition-transform ease-in duration-[0.4s] ${show===true?'scale-105':'scale-100'}`} 
+                    style={{backgroundImage:`url(http://localhost:4000/images/categories/${categoria.image})`}}/>
+            </div>
+            <div className='flex gap-2.5 items-center'>
+                <h3 className={`ml-[26px] text-sm ease-in transition-all duration-300 ${translate} ${show===true&&'text-blue-500'}`}>{categoria.name}</h3>
+                <BsArrowRight className={`ease-in transition-all duration-300 ${translate} opacity-${opacity} ${show===true&&'text-blue-500'}`}/>
+            </div>
+            </Link>
+    )
+}
--- eCommerce/src/components/NavLateral.jsx
+++ eCommerce/src/components/NavLateral.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import CategoryItem from './CategoryItem'
 
 export default function NavLateral( {categorias} ) {
 
@@ -7,13 +7,9 @@ export default function NavLateral( {categorias} ) {
     <section className='flex flex-col overflow-x-hidden mb-8 md:mb-10 mt-4 md:mt-0'>
         <div className='flex gap-6 overflow-x-auto scrollbar-hide px-6 md:px-10'>
             {categorias.map( categoria => (
-              <Link key={categoria._id} to={`/category/${categoria._id}`} className='flex flex-col items-center gap-y-3'>
-                <div className='p-1.5 bg-white rounded-[60px]'>
-                  <div className='w-32 h-32 md:w-40 md:h-40 bg-cover bg-center rounded-full' style={{backgroundImage:`url(http://localhost:4000/images/categories/${categoria.image})`}}/>
-                </div>
-                <h3 className='text-gray-600 font-medium text-sm'>{categoria.name}</h3>
-              </Link> )
-              )}
+               <CategoryItem key={categoria._id} categoria={categoria}/>
+            )
+            )}
         </div>
     </section>
   )
