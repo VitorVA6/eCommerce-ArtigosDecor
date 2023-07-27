1ebd59928d3a699bbf7e136a6daee1c47452c302
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-07-26 23:28:58 -0300

    feat: adding card options in footer

--- eCommerce/src/components/Footer.jsx
+++ eCommerce/src/components/Footer.jsx
@@ -5,6 +5,12 @@ import whats from '../assets/whats.png'
 import {FaFacebookSquare} from 'react-icons/fa'
 import {useCatalogContext} from '../contexts/Catalog'
 import {AiOutlinePlus} from 'react-icons/ai'
+import american from '../images/american.png'
+import diners from '../images/diners.png'
+import elo from '../images/elo.png'
+import hiper from '../images/hiper.png'
+import master from '../images/master.png'
+import visa from '../images/visa.png'
 
 export default function Footer() {
 
@@ -12,62 +18,85 @@ export default function Footer() {
     const classItems = 'text-[13px] text-gray-200'
 
   return (
-    <section className='flex flex-col w-full bg-gray-800 text-white'>
-        <div className='grid md:grid-cols-3 gap-y-7 gap-x-32 w-full py-12 lg:py-12 px-10 md:px-20 xl:px-32'>
+    <section className='flex flex-col w-full bg-black text-white h-fit'>
+        <div className='h-[7px] bg-gradient-to-r from-sky-500 to-indigo-500'></div>
+        <div className='flex flex-col gap-10 py-12 lg:py-12'>
 
-            <div className='flex flex-col gap-y-2'>
-                <div className='flex justify-between items-center'>
-                    <h3 className='font-medium  text-lg'>Sobre</h3>
-                    <AiOutlinePlus className='md:hidden'/>
-                </div>
-                
+            <div className='grid md:grid-cols-3 gap-y-7 w-full px-8 md:px-20 xl:px-32'>
                 <div className='flex flex-col gap-y-2'>
-                    <Link to={'/about-us'} className={classItems}>Artigos Decor</Link>
-                    <Link to={'/devolution-policy'} className={classItems}>Política de reembolso</Link>
-                    <Link to={'/privacy-policy'} className={classItems}>Política de privacidade</Link>
+                    <div className='flex justify-between items-center'>
+                        <h3 className='font-medium  text-lg'>Sobre</h3>
+                        <AiOutlinePlus className='md:hidden'/>
+                    </div>
+                    
+                    <div className='flex flex-col gap-y-2'>
+                        <Link to={'/about-us'} className={classItems}>Artigos Decor</Link>
+                        <Link to={'/devolution-policy'} className={classItems}>Política de reembolso</Link>
+                        <Link to={'/privacy-policy'} className={classItems}>Política de privacidade</Link>
+                    </div>
+                    
                 </div>
-                
-            </div>
 
-            <div className='flex flex-col gap-y-2'>
-                <div className='flex justify-between items-center'>
-                    <h3 className='font-medium  text-lg'>Atendimento</h3>
-                    <AiOutlinePlus className='md:hidden'/>
-                </div>
                 <div className='flex flex-col gap-y-2'>
-                    {
-                    catalog?.email !== '' &&
-                        <h4 className={classItems}>{catalog.email}</h4>
-                    }
-                    {
-                    catalog?.telefone !== ''  &&
-                        <h4 className={classItems}>{catalog.telefone}</h4>
-                    }
+                    <div className='flex justify-between items-center'>
+                        <h3 className='font-medium  text-lg'>Atendimento</h3>
+                        <AiOutlinePlus className='md:hidden'/>
+                    </div>
+                    <div className='flex flex-col gap-y-2'>
+                        {
+                        catalog?.email !== '' &&
+                            <h4 className={classItems}>{catalog.email}</h4>
+                        }
+                        {
+                        catalog?.telefone !== ''  &&
+                            <h4 className={classItems}>{catalog.telefone}</h4>
+                        }
+                    </div>
+                    
+                    
                 </div>
-                
-                
+            
+                <div className='flex flex-col gap-y-2'>
+                    <div className='flex justify-between items-center'>
+                        <h3 className='font-medium text-lg'>Redes sociais</h3>
+                        <AiOutlinePlus className='md:hidden'/>
+                    </div>
+                    <div className='flex flex-col gap-y-2 text-[13px]'>
+                        <Link 
+                            to={'https://www.instagram.com/artigos.decoracoes/'} 
+                            target={'_blank'} 
+                            className='gap-2 flex items-center rounded-full text-gray-200'>
+                                <img className='w-4 h-4' src={insta} alt="Logo do instagram" /> Instagram
+                        </Link>
+                        <Link 
+                            to={'https://www.instagram.com/artigos.decoracoes/'} 
+                            target={'_blank'} 
+                            className='gap-2 flex items-start rounded-full text-gray-200'>
+                                <FaFacebookSquare className='w-[16px] h-[16px] text-white'/> Facebook
+                        </Link>
+                    </div>
+                    
+                </div>
             </div>
-           
-            <div className='flex flex-col gap-y-2'>
-                <div className='flex justify-between items-center'>
-                    <h3 className='font-medium text-lg'>Redes sociais</h3>
-                    <AiOutlinePlus className='md:hidden'/>
+            <div className='flex gap-3 justify-center items-center'>
+                <div className='rounded-[3px] px-3 lg:px-3.5 py-[9px] bg-white'>
+                    <img className=' w-[18px] h-3 lg:w-6 lg:h-3.5' src={master} alt="" />
                 </div>
-                <div className='flex flex-col gap-y-2 text-[13px]'>
-                    <Link 
-                        to={'https://www.instagram.com/artigos.decoracoes/'} 
-                        target={'_blank'} 
-                        className='gap-2 flex items-center rounded-full text-gray-200'>
-                            <img className='w-4 h-4' src={insta} alt="Logo do instagram" /> Instagram
-                    </Link>
-                    <Link 
-                        to={'https://www.instagram.com/artigos.decoracoes/'} 
-                        target={'_blank'} 
-                        className='gap-2 flex items-start rounded-full text-gray-200'>
-                            <FaFacebookSquare className='w-[16px] h-[16px] text-white'/> Facebook
-                    </Link>
+                <div className='rounded-[3px] px-2 lg:px-[10px] py-[10px] bg-white'>
+                    <img className='w-[26px] h-2.5 lg:w-8 lg:h-3' src={visa} alt="" />
                 </div>
-                
+                <div className='rounded-[3px] px-[5px] lg:px[6px] py-[8px] bg-white'>
+                    <img className='w-[32px] h-3.5 lg:w-[40px] lg:h-4' src={hiper} alt="" />
+                </div>
+                <div className='rounded-[3px] px-[7px] py-[6px] bg-white'>
+                    <img className='w-[28px] h-[18px] lg:w-[32px] lg:h-5' src={american} alt="" />
+                </div>
+                <div className='rounded-[3px] px-[7px] py-[5px] bg-white'>
+                    <img className='w-[28px] h-[20px] lg:w-[32px] lg:h-[22px]' src={diners} alt="" />
+                </div>
+                <div className='rounded-[3px] px-[5px] py-[5px] bg-white'>
+                    <img className='w-[32px] h-[20px] lg:w-[38px] lg:h-[22px]' src={elo} alt="" />
+                </div>
             </div>
         </div>
         <div className='flex flex-col justify-between px-20 gap-y-8 py-6 border-t border-t-gray-500 text-xs items-center lg:px-32 lg:flex-row'>
