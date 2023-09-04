34a226e3de9fa4121a871244cca9065701e41977
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-09-04 15:51:18 -0300

    feat: create ShipOption component

---
+++ eCommerce/src/components/ShipOption.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { useCatalogContext } from '../contexts/Catalog'
+import { usePaymentContext } from '../contexts/Payment'
+import masks from '../utils/masks'
+
+export default function ShipOption({name, time, price, freight, setFreight, myFreight, width}) {
+
+  const [priceAux, setPriceAux] = useState(0)
+  const {catalog} = useCatalogContext()
+  const {formikStep2} = usePaymentContext()
+
+  function checkShipFree(price){
+    console.log(formikStep2.values.cep)
+    if(catalog.shipFree.status === true){
+        if(catalog.shipFree.validLocals === 'ALL'){
+            return 0
+        }
+        else if(catalog.shipFree.validLocals === 'STATE' && formikStep2.values.estado === catalog.address.estado){
+            return 0
+        }
+        else if(catalog.shipFree.validLocals === 'CITY' && formikStep2.values.cidade === catalog.address.cidade){
+            return 0
+        }
+    }
+    return price
+  }
+
+  function handleClick(){
+    setFreight(
+      {
+          delivery: myFreight,
+          price: priceAux
+      })
+  }
+
+  useEffect(() => {
+    setPriceAux(checkShipFree(price))
+  }, [formikStep2.values.cidade])
+
+  return (
+    <div 
+      className={`cursor-pointer flex text-gray-500 ${width} rounded-r-md justify-between items-center border-l-4 ${freight.delivery === myFreight ? 'bg-blue-100 border-l-blue-500' : 'bg-gray-100 border-l-gray-300'} px-5 py-2`}
+      onClick={handleClick}
+    >
+        <div className='flex flex-col justify-center'>
+            <h3 className='font-medium'>{name}</h3>
+            <p className='text-sm -mt-0.5'>{time}</p>
+        </div>
+        <p className='text-sm font-medium'>{masks.maskCurrency(priceAux)}</p>
+    </div>
+  )
+}
